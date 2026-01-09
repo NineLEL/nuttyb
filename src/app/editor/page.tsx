6af9ba742b105b4f65d27d5ec1015f12d2d48bcb
timuela <kevingom43@gmail.com>
2026-01-09 11:37:12 +0700

    renamed

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -5,7 +5,7 @@ import { Stack, Text, Title } from '@mantine/core';
 import { useConfiguratorContext } from '@/components/contexts/configurator-context';
 import { useLuaBundleContext } from '@/components/contexts/lua-bundle-context';
 import PageLoader from '@/components/page-loader';
-import LuaEditor from '@/components/tabs/editor/lua-editor';
+import Editor from '@/components/tabs/editor/editor';
 
 export default function Page() {
     const { luaFiles, isLoading: isLuaLoading, error } = useLuaBundleContext();
@@ -26,7 +26,7 @@ export default function Page() {
                 </Text>
             </Stack>
 
-            <LuaEditor luaFiles={luaFiles} configuration={configuration} />
+            <Editor luaFiles={luaFiles} configuration={configuration} />
         </Stack>
     );
 }
